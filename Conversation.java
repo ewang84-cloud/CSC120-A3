04e5f8ecf19a5c0cdd7f459d4ebbbb56aff56606
ewang84-cloud <ewang84@smith.edu>
2025-09-28 23:40:54 -0400

    A3 09.28

--- Conversation.java
+++ Conversation.java
@@ -1,28 +1,148 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
 
 class Conversation implements ConversationRequirements {
 
-  // Attributes 
+  /**
+   * Attributes
+   */
+  private int NumIntended;
+  private int NumFinished=0;
+  private ArrayList<String> Transcript;
+  private Scanner scanner;
+  private Random random;
+  private String[] responses;
+  private Map<String, String> wordMap;
+
 
   /**
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation() {
+    this.NumIntended = 0;
+    this.NumFinished = 0;
+    this.Transcript = new ArrayList<>();
+    this.scanner = new Scanner (System.in);
+    this.random = new Random();
+    this.responses = new String[]{
+      "Hmmm...",
+      "Sounds interesting!",
+      "Really?",
+      "Uh-uhu",
+    };
+    this.wordMap = new HashMap<>();
+      wordMap.put("I", "you");
+      wordMap.put("me", "you");
+      wordMap.put("am", "are");
+      wordMap.put("you", "I");
+      wordMap.put("my", "your");
+      wordMap.put("your", "my");
   }
+
+
+  /**
+   * Ask for the number of 
+   */
+  public void RoundNum(){
+    System.out.println("How many rounds?");
+    NumIntended = scanner.nextInt();
+    scanner.nextLine();
+    System.out.println();
+  }
+  
+
+/**
+ * Responding the random responses for message
+ * @param inputString
+ * @return random responses
+ */
+  public String RandomRespond (String inputString){
+    int TempNum = random.nextInt(responses.length);
+    return responses[TempNum];
+  }
+
+
+  /**
+   * Turning the targeted sentences into the responding version
+   * @param input
+   * @return the sentence of response without question mark
+   */
+  private String convertWords(String input) {
+    String[] words = input.split("\\s+");
+    StringBuilder result = new StringBuilder();
+    
+    for (int i = 0; i < words.length; i++) {
+      String word = words[i];
+      if (wordMap.containsKey(word)) {
+        result.append(wordMap.get(word));
+      }
+      else {
+        result.append(word);
+      }
+
+      if (i < words.length - 1) {
+        result.append(" ");
+      }
+    }
+    
+    return result.toString();
+  }
+
+
+  /**
+   * Responding the converted response back with a question mark
+   * @param inputString
+   * @return whole converted response
+   */
+  public String TargetResponse(String inputString) {
+    String converted = convertWords(inputString);
+    return converted + "?";
+  } 
+
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    if (NumFinished == 0){
+      String Beginning = ("Welcome to ChatBox! What's on your mind?");
+      System.out.println(Beginning);
+      Transcript.add(Beginning);
+    } 
 
-  }
+    while (NumFinished < NumIntended){
+      String userInput = scanner.nextLine();
+      Transcript.add(userInput);         
+      String response = respond(userInput);
+      Transcript.add(response);
+      System.out.println(response);
+
+      NumFinished ++;
+      }
+    
+    if (NumFinished == NumIntended){
+      String Ending = ("See ya!");
+      System.out.println(Ending);
+      Transcript.add(Ending);
+      
+    }
+    }
+  
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    if(NumFinished == NumIntended){
+      System.out.println ("TRANSCRIPT:");
+      for (String message : Transcript)
+        System.out.println (message);
+    }
   }
+
 
   /**
    * Gives appropriate response (mirrored or canned) to user input
@@ -30,13 +150,26 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    boolean shouldMirror = false;
+  for (String key : wordMap.keySet()) {
+    if (inputString.contains(key)) {
+      shouldMirror = true;
+      break;
+    }
+    }
+    
+  if (shouldMirror) {
+    return TargetResponse(inputString);
+  }
+  else{
+    return RandomRespond(inputString);
+  }
   }
 
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
+    myConversation.RoundNum();
     myConversation.chat();
     myConversation.printTranscript();
 
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Cola Shi, Amy Tang
 - Which session(s) of TA / office hours did you attend?
+    Intended to go to the TA of Kira on Monday to ask more about interface
 - What are your initial impressions of Java? 
+    More gramatical requirement, also a lot of {}s... but generally liked it!!
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    A before working brainstorm really helps... no matter the language
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    the structure, and the thinking process of the prokect is very important!! Think of that first before doing anything specific, or think while doing
